50f9303026c46e2ca903cae6cb9b08c657b8f072
kys6879 <kys6879@naver.com>
2019-05-12 04:41:10 +0900

    GOAL 추가

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 declare var $: any;
 
 @Component({
@@ -7,11 +12,36 @@ declare var $: any;
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-
-  constructor() { }
+  createForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.createForm = this.formBuilder.group({
+      goalName: ['', []],
+      goalPrice: ['', []],
+    });
     $('body').css("background-color", "#f7f7f7");
+  }
+
+  setGoal() {
+    let param = {
+      title: this.createForm.value.goalName,
+      goal_price: parseInt(this.createForm.value.goalPrice, 10)
+    };
+
+    console.log("param", param);
+
+    this.http.post(`${environment.server.url}/api/users/goals`, param, {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`),
+    })
+      .subscribe(ret => {
+        $("#centralModalSuccess").modal('toggle').on('hidden.bs.modal', (e) => {
+          this.router.navigate(['/withdraw']);
+        });
+        console.log("register()", ret);
+        console.log("param ", param);
+      });
+
   }
 
 }
--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 declare var $: any;
 
 @Component({
@@ -8,10 +12,21 @@ declare var $: any;
 })
 export class WithdrawComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     $('body').css("background-color", "#f7f7f7");
+
+    this.getGoals();
+  }
+
+  getGoals() {
+    this.http.get(`${environment.server.url}/api/users/goals`, {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`),
+    })
+      .subscribe(ret => {
+        console.log("ret()", ret);
+      });
   }
 
 }
