9c79bbd4a5f46eca92513e2c93d428f5bd4a3ed5
kys6879 <kys6879@naver.com>
2019-05-11 15:42:38 +0900

    회원가입 화면

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  authForm: string = "login";
+  bodyColor;
   constructor() { }
 
   ngOnInit() {
+    this.bodyColor = $('body').css("background-color");
+    console.log(this.bodyColor);
+  }
+
+  changeAuthForm(form: string) {
+    this.authForm = form;
+
+    if (this.authForm == "register") {
+      console.log("asdf");
+      $('body').css("background-color", "#f7f7f7");
+    } else {
+      $('body').css("background-color", this.bodyColor);
+    }
   }
 
 }
