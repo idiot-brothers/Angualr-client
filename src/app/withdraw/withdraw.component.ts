3d3a7474a48e7df14fed41b9ef52ac05bf54a8e9
kys6879 <kys6879@naver.com>
2019-05-12 05:51:41 +0900

    달성치 구현

--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { finalize, take, map, tap } from 'rxjs/operators';
+import { MoneyService } from '../services/money.service'
 import { Router } from '@angular/router';
 declare var $: any;
 
@@ -12,7 +15,9 @@ declare var $: any;
 })
 export class WithdrawComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+  goalsObservable$: Observable<any>;
+  goalPercent: number = 0;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private moneyService: MoneyService) { }
 
   ngOnInit() {
     $('body').css("background-color", "#f7f7f7");
@@ -21,12 +26,27 @@ export class WithdrawComponent implements OnInit {
   }
 
   getGoals() {
-    this.http.get(`${environment.server.url}/api/users/goals`, {
+    this.goalsObservable$ = this.http.get(`${environment.server.url}/api/users/goals`, {
       headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`),
     })
-      .subscribe(ret => {
-        console.log("ret()", ret);
-      });
+      .pipe(
+        map((data: any) => {
+
+          data.forEach((d: any) => {
+            d.goal_price = this.moneyService.today * 100 / d.goal_price;
+            d.goal_price_int = parseInt(d.goal_price, 10);
+          });
+          // console.log(d);
+          // console.log(parseInt(d.goal_price, 10) * 100);
+          // console.log('goal :', d.goal_price * 100 / this.moneyService.week);
+          return data;
+        }),
+        tap(data => console.log('[orderer-list.componenet] #queryOrdererServer - data:', data)),
+      )
+  }
+
+  withdraw() {
+    $("#centralModalDanger").modal('toggle');
   }
 
 }
